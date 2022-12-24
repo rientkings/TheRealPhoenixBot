a90f2e367d99fd14a82aea666ce0021e1a9d1bbf
Rient <121344001+Rientcoder@users.noreply.github.com>
2022-12-25 00:44:55 +0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -24,13 +24,13 @@ Hi {}, my name is {}!
 You can find the list of available commands with /help.
 """
 
-BOT_IMAGE = "https://telegra.ph/file/93612a540608640355f20.mp4"
+BOT_IMAGE = "https://telegra.ph/file/37308f371ae6b941258fc.jpg"
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
-If you have any questions on how to use me, head over to @PhoenixSupport
+If you have any questions on how to use me, head over to @ayakanetwork
 
 *Main* commands available:
  - /start: start the bot
